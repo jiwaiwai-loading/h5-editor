a5c1ae78766b217dec9a2889888f463adb22b883
wu7zhi <wu7zhi@qq.com>
2024-01-30 14:29:26 +0800

    build mock

--- vite.config.js
+++ vite.config.js
@@ -32,7 +32,7 @@ export default defineConfig(({
         resolvers: [ElementPlusResolver()],
       })
     ],
-    base: '/h5-editor/dist/',
+    base: '/h5-editor/',
     minify: true,
     sourceMap: true,
     resolve: {
